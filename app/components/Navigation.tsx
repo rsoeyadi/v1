39bfcaf60cb9b3c3dd8a3d03e9e0e7ca8da3f259
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-08 23:26:46 -0400

    cursor pointer for navbar

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -4,7 +4,10 @@ import { Link } from "react-scroll";
 
 const Navigation = () => {
   return (
-    <nav className="nav lg:block" aria-label="In-page jump links">
+    <nav
+      className="nav lg:block cursor-pointer"
+      aria-label="In-page jump links"
+    >
       <ul className="mt-10 w-max">
         {["about", "experience", "projects"].map((section) => (
           <li key={section}>
@@ -14,7 +17,7 @@ const Navigation = () => {
               smooth={true}
               offset={50}
               duration={500}
-              className="group flex items-center py-3 active"
+              className="cursor-pointer group flex items-center py-3 active"
             >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
               <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
