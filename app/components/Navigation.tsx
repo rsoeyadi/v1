eb65aba47231c550b93f2d107fa677b57c19a9b9
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-08 23:21:43 -0400

    scrolling library works

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,31 +1,55 @@
+"use client";
+import { Link } from "react-scroll";
+
 export default function Navigation() {
   return (
     <>
       <nav className="nav lg:block" aria-label="In-page jump links">
         <ul className="mt-10 w-max">
-          <li className="">
-            <a className="group flex items-center py-3 active" href="#about">
+          <li>
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              className="group flex items-center py-3 active"
+            >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
               <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 About
               </span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="group flex items-center py-3" href="#experience">
+            <Link
+              to="experience"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              className="group flex items-center py-3 active"
+            >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
               <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 Experience
               </span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a className="group flex items-center py-3" href="#projects">
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              className="group flex items-center py-3 active"
+            >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
               <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
                 Projects
               </span>
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
