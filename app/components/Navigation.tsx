ef2b33ffb80683e8ac9889c44759a64a14313c1a
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-08 23:22:54 -0400

    map to create each navigation link instead

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,14 +1,15 @@
 "use client";
+import React from "react";
 import { Link } from "react-scroll";
 
-export default function Navigation() {
+const Navigation = () => {
   return (
-    <>
-      <nav className="nav lg:block" aria-label="In-page jump links">
-        <ul className="mt-10 w-max">
-          <li>
+    <nav className="nav lg:block" aria-label="In-page jump links">
+      <ul className="mt-10 w-max">
+        {["about", "experience", "projects"].map((section) => (
+          <li key={section}>
             <Link
-              to="about"
+              to={section}
               spy={true}
               smooth={true}
               offset={50}
@@ -17,42 +18,14 @@ export default function Navigation() {
             >
               <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
               <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                About
+                {section.charAt(0).toUpperCase() + section.slice(1)}
               </span>
             </Link>
           </li>
-          <li>
-            <Link
-              to="experience"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              className="group flex items-center py-3 active"
-            >
-              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                Experience
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-              className="group flex items-center py-3 active"
-            >
-              <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-              <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-                Projects
-              </span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+        ))}
+      </ul>
+    </nav>
   );
-}
+};
+
+export default Navigation;
