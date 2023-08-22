e4f24519c295f0128e350b4329316d819e0135c2
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-22 00:32:04 -0400

    default theme

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -3,5 +3,9 @@
 import { ThemeProvider } from "next-themes";
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light">
+      {children}
+    </ThemeProvider>
+  );
 }
