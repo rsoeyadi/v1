c9700a42e1df4b2caace589bf502070c9e63851b
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-22 22:20:39 -0400

    removed comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,19 +13,10 @@ module.exports = {
   theme: {
     screens: {
       sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
       md: "860px",
-      // => @media (min-width: 830px) { ... }
-
       lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
       xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
       "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
     },
   },
 };
