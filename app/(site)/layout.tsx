70667ef0b3e929a521cf07a668ed6ed325b41346
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-08-25 14:41:02 -0400

    no index

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -50,6 +50,19 @@ export const metadata: Metadata = {
       },
     ],
   },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default async function RootLayout({
